c096fd5cfa0dc21a185bd3884da7b3362902e9ec
shrawank22 <shrawan8565@gmail.com>
2024-02-21 17:56:25 +0530

    ui-updates

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import sharedData from './assets/portfolio_shared_data.json';
-import resumeData from './assets/res_primaryLanguage.json'
+import resumeData from './assets/resume_data.json'
 
 const App = () => {
   return (
